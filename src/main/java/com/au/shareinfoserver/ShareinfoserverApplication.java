1e32a1faf035065e5f837b473489278ab8f8b3cc
Hao Xu <xuhaoprofessor@gmail.com>
2017-10-09 20:43:02 +0800

    Init share info server project.

---
+++ src/main/java/com/au/shareinfoserver/ShareinfoserverApplication.java
@@ -0,0 +1,12 @@
+package com.au.shareinfoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareinfoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareinfoserverApplication.class, args);
+	}
+}
