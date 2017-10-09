2753c93ec8e5d39e1b22b3f715bd5485dc3d9011
Hao Xu <xuhaoprofessor@gmail.com>
2017-10-09 20:51:39 +0800

    Create hello world controller.

---
+++ src/main/java/com/au/shareinfoserver/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.au.shareinfoserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello world";
+    }
+
+}
