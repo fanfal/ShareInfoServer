9246fdd3c4693e0c32d151693cabeb80bc81f4e0
Hao Xu <xuhaoprofessor@gmail.com>
2017-10-16 14:26:19 +0800

    Add setting of data in poperty file.

--- src/main/java/com/au/shareinfoserver/dao/CarInfoRepository.java
+++ src/main/java/com/au/shareinfoserver/dao/CarInfoRepository.java
@@ -3,7 +3,9 @@ package com.au.shareinfoserver.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public interface CarInfoRepository extends JpaRepository<CarInfo, Long > {
 }
