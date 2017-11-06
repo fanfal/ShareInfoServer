9eab318720b46b61219f963365a7abc84889378d
Hao Xu <xuhaoprofessor@gmail.com>
2017-11-06 18:11:48 +0800

    Reuqets user join with message.

--- src/main/java/com/au/shareinfoserver/dao/User.java
+++ src/main/java/com/au/shareinfoserver/dao/User.java
@@ -1,6 +1,7 @@
 package com.au.shareinfoserver.dao;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Set;
 
 @Entity
@@ -19,7 +20,7 @@ public class User {
     private Integer cash = 0;
 
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "message_phone_num")
+    @JoinColumn(name = "phoneNum", referencedColumnName = "phoneNum")
     private Set<Message> messages;
 
     public Integer getId() {
